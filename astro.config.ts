60b055fb3771c0c24e25063a47946462d159a776
Nikita Gaziev <zobweyt@gmail.com>
2025-03-26 16:30:59 +0300

    style: format code

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,5 @@
 import mdx from "@astrojs/mdx";
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
