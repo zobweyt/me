95abab5a66a73d0330b82c59dd666f26be3af482
Nikita Gaziev <zobweyt@gmail.com>
2025-03-31 14:47:54 +0300

    feat: add rss link to footer and colorize links

--- src/constants.ts
+++ src/constants.ts
@@ -4,17 +4,21 @@ export const RECENT_PROJECTS_ENTRIES_COUNT = 4;
 export const SOCIALS: {
   name: string;
   href: string;
+  color?: string;
 }[] = [
   {
     name: "telegram",
     href: "https://t.me/zobweyt",
+    color: "#0088CC",
   },
   {
     name: "github",
     href: "https://github.com/zobweyt",
+    color: "#181717",
   },
   {
     name: "zobweyt@gmail.com",
     href: "mailto:zobweyt@gmail.com",
+    color: "#D93025",
   },
 ];
