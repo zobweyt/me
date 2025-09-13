c568d17921826f233ffe53f4e61397ab48ebca3e
Nikita Gaziev <zobweyt@gmail.com>
2025-09-13 22:08:45 +0300

    feat: improve adaptivity of cmdk search

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -36,7 +36,7 @@ export const DialogContent = ({ className, children, ...props }: React.Component
       <DialogOverlay />
       <Content
         className={cn(
-          "fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-xl border border-black/15 bg-stone-100 p-6 shadow data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[state=closed]:slide-out-to-top-4 data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95 data-[state=open]:slide-in-from-top-4 sm:max-w-lg sm:data-[state=closed]:animate-out sm:data-[state=open]:animate-in",
+          "fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-xl bg-stone-100 p-6 shadow data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[state=closed]:slide-out-to-top-4 data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95 data-[state=open]:slide-in-from-top-4 sm:max-w-lg sm:border sm:border-black/15 sm:data-[state=closed]:animate-out sm:data-[state=open]:animate-in",
           className,
         )}
         {...props}
