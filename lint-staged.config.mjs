f58cfd8b0d25ff0fe74a3a7ecb2b9b3769872436
Nikita Gaziev <zobweyt@gmail.com>
2025-09-15 17:19:59 +0300

    feat: improve project

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,7 +1,5 @@
 export default {
-  "*.{astro,js,jsx,ts,tsx}": (stagedFiles) => [
-    `pnpm lint:eslint ${stagedFiles.join(" ")}`,
-  ],
+  "*.{astro,js,jsx,ts,tsx}": (stagedFiles) => [`pnpm lint:eslint ${stagedFiles.join(" ")}`],
   "*.{astro,js,jsx,ts,tsx,css,scss,less,html,json,md}": (stagedFiles) => [
     `pnpm format:prettier:write ${stagedFiles.filter((file) => !file.includes("/public/")).join(" ")}`,
   ],
