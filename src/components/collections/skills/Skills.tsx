45c5f7c6667ac8a847eca4cec7710564ee3eae84
Nikita Gaziev <zobweyt@gmail.com>
2026-05-08 15:04:39 +0000

    feat: add autosize to skills

--- src/components/collections/skills/Skills.tsx
+++ src/components/collections/skills/Skills.tsx
@@ -1,11 +1,111 @@
 import { ScrollArea } from "@base-ui/react/scroll-area";
 import { Tabs } from "@base-ui/react/tabs";
 import { cx } from "class-variance-authority";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { useWebHaptics } from "web-haptics/react";
 import SKILLS_CATEGORIES from "@/content/skills/categories.json";
 import SKILLS_GROUPS from "@/content/skills/groups.json";
 import type { Skill } from "@/lib/collections/skills";
+
+export const useAutoResize = ({
+  property = "height",
+}: {
+  property?: "height" | "width" | "both";
+}) => {
+  // ref can be attached to any HTML element
+  const ref = useRef<HTMLDivElement>(null);
+  const [height, setHeight] = useState<number | "auto">("auto");
+  const [width, setWidth] = useState<number | "auto">("auto");
+
+  useEffect(() => {
+    if (ref.current) {
+      const resizeObserver = new ResizeObserver((entries) => {
+        const observedHeight = entries[0].contentRect.height;
+        const observedWidth = entries[0].contentRect.width;
+        setHeight(observedHeight);
+        setWidth(observedWidth);
+      });
+
+      resizeObserver.observe(ref.current);
+
+      return () => {
+        resizeObserver.disconnect();
+      };
+    }
+  }, []);
+
+  const heightValue = property === "height" ? height : "auto";
+  const widthValue = property === "width" ? width : "auto";
+
+  return {
+    ref,
+    height: heightValue,
+    width: widthValue,
+  };
+};
+
+const QUERY = "(prefers-reduced-motion: reduce)";
+
+export function usePrefersReducedMotion() {
+  const [prefersReducedMotion, setPrefersReducedMotion] = useState(() =>
+    typeof window !== "undefined" ? window.matchMedia(QUERY).matches : false,
+  );
+
+  useEffect(() => {
+    const mediaQueryList = window.matchMedia(QUERY);
+    const handleChange = (e: MediaQueryListEvent) =>
+      setPrefersReducedMotion(e.matches);
+
+    mediaQueryList.addEventListener("change", handleChange);
+    return () => mediaQueryList.removeEventListener("change", handleChange);
+  }, []);
+
+  return prefersReducedMotion;
+}
+
+export const AutoResize = ({
+  children,
+  overflow = false,
+  duration = 400,
+  className,
+  property = "height",
+}: {
+  children: React.ReactNode;
+  overflow?: boolean;
+  duration?: number;
+  className?: string;
+  property?: "height" | "width" | "both";
+}) => {
+  const { ref, width, height } = useAutoResize({
+    property,
+  });
+  const [mounted, setMounted] = useState(false);
+  const prefersReducedMotion = usePrefersReducedMotion();
+  const transitionProperty = property === "both" ? "height, width" : property;
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return (
+    <div
+      style={{
+        height,
+        width,
+        overflow: overflow ? "visible" : "hidden",
+        transitionDuration: mounted
+          ? prefersReducedMotion
+            ? "0ms"
+            : `${duration}ms`
+          : "0ms",
+        transitionTimingFunction: "cubic-bezier(0.19, 1, 0.22, 1)",
+        transitionProperty,
+      }}
+      className={className}
+    >
+      <div ref={ref}>{children}</div>
+    </div>
+  );
+};
 
 export default function Skills({
   skills,
@@ -120,44 +220,46 @@ export default function Skills({
           </ScrollArea.Content>
         </ScrollArea.Viewport>
       </Tabs.List>
-      <Tabs.Panel
-        key={selectedCategory}
-        value={selectedCategory}
-        tabIndex={-1}
-        className="grid grid-cols-2 mt-4"
-      >
-        {entries.map(([group, skills], index) => (
-          <section
-            key={group}
-            className={cx(
-              "p-4 sm:px-4 lg:px-8 border-t border-foreground/5",
-              index % 2 !== 0 && "border-l",
-            )}
-          >
-            <h3 className="text-xs font-mono light:font-medium uppercase tracking-widest text-current/50 mb-1.5">
-              {group}
-            </h3>
-            <ul className="flex flex-wrap gap-2 max-sm:gap-1">
-              {skills?.map((skill) => (
-                <li key={skill.id}>
-                  <a
-                    title={skill.data.name}
-                    className={cx(
-                      "flex size-7! focus-visible:scale-110 @hover:scale-110 active:opacity-75 motion-safe:transition outline-none",
-                      skill.data.icon,
-                    )}
-                    rel="noopener noreferrer"
-                    target="_blank"
-                    href={skill.data.href}
-                  >
-                    <span className="sr-only">{skill.data.name}</span>
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </section>
-        ))}
-      </Tabs.Panel>
+      <AutoResize>
+        <Tabs.Panel
+          key={selectedCategory}
+          value={selectedCategory}
+          tabIndex={-1}
+          className="grid grid-cols-2 pt-4"
+        >
+          {entries.map(([group, skills], index) => (
+            <section
+              key={group}
+              className={cx(
+                "p-4 sm:px-4 lg:px-8 border-t border-foreground/5",
+                index % 2 !== 0 && "border-l",
+              )}
+            >
+              <h3 className="text-xs font-mono light:font-medium uppercase tracking-widest text-current/50 mb-1.5">
+                {group}
+              </h3>
+              <ul className="flex flex-wrap gap-2 max-sm:gap-1">
+                {skills?.map((skill) => (
+                  <li key={skill.id}>
+                    <a
+                      title={skill.data.name}
+                      className={cx(
+                        "flex size-7! focus-visible:scale-110 @hover:scale-110 active:opacity-75 motion-safe:transition outline-none",
+                        skill.data.icon,
+                      )}
+                      rel="noopener noreferrer"
+                      target="_blank"
+                      href={skill.data.href}
+                    >
+                      <span className="sr-only">{skill.data.name}</span>
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </section>
+          ))}
+        </Tabs.Panel>
+      </AutoResize>
     </Tabs.Root>
   );
 }
