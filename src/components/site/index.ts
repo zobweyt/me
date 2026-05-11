5b9ebb6d6916967bc1894160f3f2657e11c90238
Nikita Gaziev <zobweyt@gmail.com>
2026-05-11 12:58:20 +0000

    feat: add footer

--- src/components/site/index.ts
+++ src/components/site/index.ts
@@ -1,3 +1,4 @@
+export { default as Footer } from "./Footer.astro";
 export { default as Head } from "./Head.astro";
 export { default as Header } from "./Header.astro";
 export { default as Search } from "./Search";
